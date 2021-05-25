a999401e71b1a8e16f5e49a2cda5cbf1df4f86db
Ralph Aguiar <ralphaguiar@gmail.com>
2021-05-25 19:40:49 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,12 +1,17 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,9 +23,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	//@JsonIgnore
-	//@ManyToMany(mappedBy = "categories")
-	//private Set<Product> products = new HashSet<>();
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
